526286f05f1b0451537a5dc9baf59cd004b58d44
Alireza Karbalayi Morad <alireza.version@gmail.com>
2022-01-06 15:57:35 +0330

    first commit of project

---
+++ src/components/Header.js
@@ -0,0 +1,27 @@
+import PropTypes from 'prop-types'
+import Button from "./Button"
+const Header = ({title}) => {
+    const onClick = () => {
+        console.log('adsf');
+    }
+    return (
+        <header className='header'>
+            <h1>{title}</h1>
+            <Button color = 'green' text = 'Add' onClick = {onClick} />
+        </header>
+    )
+}
+Header.defaultProps = {
+    title :'Task Manager'
+}
+Header.propTypes = {
+    title : PropTypes.string.isRequired
+}
+
+// css in js
+// const headingStyles = {
+//     color : 'red',
+//     backgroundColor : 'blue'
+// }
+
+export default Header
